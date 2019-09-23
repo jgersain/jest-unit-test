ac2b4db3a4331c0b6c6b2f25da4f45048fd2b8ee
jgersain <jhersein@gmail.com>
2019-09-23 12:23:27 -0500

    probando callbacks

---
+++ __test__/callbacks.test.js
@@ -0,0 +1,13 @@
+import {
+  callbackHell
+} from '../callbacks';
+
+describe('Probando un callback', () => {
+  test('Callback', done => {
+    function otherCallback(data) {
+      expect(data).toBe('hola javascripters');
+      done();
+    };
+    callbackHell(otherCallback);
+  })
+})
