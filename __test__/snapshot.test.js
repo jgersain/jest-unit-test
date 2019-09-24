6f2d529fbc4b2cdc90a58dd647cf3b7fbea26bdc
jgersain <jhersein@gmail.com>
2019-09-24 00:15:52 -0500

    probando excepciones con snapshots

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,5 +5,23 @@ describe("Instantaneas", () => {
   test('Snapshot', () => {
     expect(getCharacter(rick)).toMatchSnapshot();
   });
+
+  test('Siempre fallara la instantanea', () => {
+    const user = {
+      createAt: new Date(),
+      id: Math.floor(Math.random() * 20),
+    }
+    expect(user).toMatchSnapshot();
+  });
+
+  test('Permitir excepciones dentro del código (snapshots)', () => {
+    const user = {
+      name: "Oscar Barajas",
+      id: Math.floor(Math.random() * 20),
+    }
+    expect(user).toMatchSnapshot({
+      id: expect.any(Number)
+    });
+  })
 });
   
